204bc2b6920448d2d041248e3359c9828d1a1713
Jason Wilkens <jasonwilkens@bizo.com>
2016-10-22 11:27:05 -0700

    fixed some bugs

--- js/app.js
+++ js/app.js
@@ -1,23 +1,3 @@
-//Vue.component('nav', {
-//  template: '#nav'
-//})
-//
-//Vue.component('home', {
-//  template: '#home'
-//});
-//
-//Vue.component('work', {
-//  template: '#work'
-//});
-//
-//Vue.component('process', {
-//  template: '#process'
-//});
-//
-//Vue.component('about', {
-//  template: '#about'
-//});
-
 const Home = { template: '#home' };
 const Work = { template: '#work' };
 const Process = { template: '#process' };
