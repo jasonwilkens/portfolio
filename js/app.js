c700aaaa7fc8d5948a51e923b7775f06aad19b25
Jason Wilkens <jason@linkedin.com>
2016-11-04 17:25:09 -0700

    starting to add project-nav routing

--- js/app.js
+++ js/app.js
@@ -1,38 +1,56 @@
 // Vue Routes
 
-var Home = { template: '#home' };
-var Work = { template: '#work' };
-var Process = { template: '#process' };
-var About = { template: '#about' };
-
-var routes = [
-  { path: '/', component: Home },
-  { path: '/work', component: Work },
-  { path: '/process', component: Process },
-  { path: '/about', component: About }
-];
-
-var router = new VueRouter({
-  routes
-});
+var home = { template: '#home' },
+    work = { template: '#work' },
+    process = { template: '#process' },
+    about = { template: '#about' },
+    leadAccelerator = { template: '#lead-accelerator' }
+    routes = [
+      { path: '/', component: home, meta: { project: false } },
+      { path: '/work', component: work, meta: { project: false } },
+      { path: '/process', component: process, meta: { project: false } },
+      { path: '/about', component: about, meta: { project: false } },
+      { path: '/work/lead-accelerator', component: leadAccelerator, meta: { project: true }}
+    ],
+    router = new VueRouter({
+      routes
+    });
 
 router.afterEach(function () {
   scroll(0,0);
 });
 
+// Nav
+
+var isProjectVariable,
+    projectTitle
+    projectTitleMap = {
+      leadAccelerator: 'Lead Accelerator'
+    };
+
+router.beforeEach(function (to, from, next) {
+  isProjectVariable = to.meta.project;
+  next();
+});
+
+Vue.component('nav-bar', {
+  props: ['isProjectProp'],
+  template: '#nav'
+});
+
 // Greeting
 
-var date = new Date();
-var hour = date.getHours();
-var greeting = function () {
-  if (hour < 12) {
-    return 'Good morning';
-  } else if (hour < 18) {
-    return 'Good afternoon';
-  } else {
-    return 'Good evening';
-  }
-};
+var date = new Date(),
+    hour = date.getHours(),
+    greeting = function () {
+      if (hour < 12) {
+        return 'Good morning';
+      } else if (hour < 18) {
+        return 'Good afternoon';
+      } else {
+        return 'Good evening';
+      }
+    };
 
 Vue.component('greeting', {
   template: '<h2>{{ message }}</h2>',
@@ -45,10 +63,11 @@ Vue.component('greeting', {
 
 // Create Vue App
 
-var app = new Vue({ router }).$mount('#app');
+var app = new Vue({ router, data: { isProjectRoot: isProjectVariable }, }).$mount('#app');
 
 // Headroom JS
 
-var navElement = document.querySelector('.nav-bar');
-var headroom = new Headroom(navElement);
+var navElement = document.querySelector('.nav-bar'),
+    headroom = new Headroom(navElement);
+
 headroom.init();
