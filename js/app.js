25e5e88fecd65fbc875f0375af99abc1ed79db4e
Jason Wilkens <jason@linkedin.com>
2017-05-19 10:49:39 -0700

    scroll position

--- js/app.js
+++ js/app.js
@@ -32,7 +32,14 @@ var home = { template: '#home' },
     ],
     router = new VueRouter({
       mode: 'history',
-      routes: routes
+      routes: routes,
+      scrollBehavior (to, from, savedPosition) {
+        if (savedPosition) {
+          return savedPosition
+        } else {
+          return { x: 0, y: 0 }
+        }
+      }
     }),
     campaigns = [
       {
